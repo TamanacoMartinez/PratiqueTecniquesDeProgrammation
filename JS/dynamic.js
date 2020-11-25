c00b85046bc5d3a3bed48004677a8c7a1c7aca56
Tamanaco Martínez <tamanacomb@gmail.com>
2020-11-24 21:36:32 -0500

    Exercise Mon Site Web

---
+++ JS/dynamic.js
@@ -0,0 +1,9 @@
+function ValeurDeMaVariable () {
+       
+    var entier = 3
+     
+     if (entier == 5)
+     {alert ("la variable vaut 5");}
+    
+     else {alert ("la variable ne vaut pas 5");}
+ }
